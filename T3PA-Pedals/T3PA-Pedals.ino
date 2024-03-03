679b49e1a44d20fc6109e747dc6729d07d29fec6
adrianmarino <adrianmarino@gmail.com>
2024-03-03 10:57:29 -0300

    feat: remoe smoothin. it was replaced by 0.2 µF capasitor conencte on each input

--- T3PA-Pedals/T3PA-Pedals.ino
+++ T3PA-Pedals/T3PA-Pedals.ino
@@ -1,17 +1,11 @@
 #include <Joystick.h>
-#include <Ewma.h>
+#include <ArduinoJson.h>
 
 Joystick_ Joystick;
 
 #define CLUTCH A0
-#define BREAK  A1
-#define GAS    A2
-#define SAMPLE_SIZE 10
-
-Ewma clutchFilter(0.9);
-Ewma breakFilter(0.9);
-Ewma gasFilter(0.9);
-
+#define BREAK A1
+#define THROTTLE A2
 
 int readAnalogInput(int pin, int delayValue=1) {
   int value = map(analogRead(pin), 0, 1023, 0, 255);
@@ -24,13 +18,8 @@ void setup() {
 }
 
 void loop(){
-  int clutchValue = readAnalogInput(CLUTCH);
-  int breakValue  = readAnalogInput(BREAK);
-  int gasValue    = readAnalogInput(GAS);
-
-  Joystick.setZAxis(int(clutchFilter.filter(clutchValue))); 
-  Joystick.setRxAxis(int(breakFilter.filter(breakValue)));
-  Joystick.setThrottle(int(gasFilter.filter(gasValue)));
-
+  Joystick.setZAxis(readAnalogInput(CLUTCH)); 
+  Joystick.setRxAxis(readAnalogInput(BREAK));
+  Joystick.setThrottle(readAnalogInput(THROTTLE));
   delay(1);
 }
